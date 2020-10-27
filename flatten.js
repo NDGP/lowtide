44ee1b6ee3c335344531c6770f2eaac31365dcbd
Nathan <nathanpatrick939@gmail.com>
2020-10-27 23:12:07 +0000

    flatten the array

---
+++ flatten.js
@@ -0,0 +1,15 @@
+const flatten = (arr) => {
+  let newArr = []
+  for (let i = 0; i < arr.length; i++){
+    if (Array.isArray(arr[i])){
+      let insideArr = arr[i];
+    for (let k = 0; k < insideArr.length; k++){
+        newArr.push(insideArr[k]);
+      }
+    }else{newArr.push(arr[i])}
+  }
+  return newArr
+}
+
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]))
