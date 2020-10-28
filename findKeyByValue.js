b641a9e18fca6734b126b9d1a5eb8995b14c449a
Nathan <nathanpatrick939@gmail.com>
2020-10-28 20:16:39 +0000

    this one was easy

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected){
+  console.log(`Assertion Passed: [${actual}] === [${expected}]`)
+  }else
+  {console.log(`Assertion Failed: [${actual}] !== [${expected}]`)}
+};
+
+
+
+
+const findKeyByValue = (bestTVShowsByGenre, show) =>{
+  for (let key in bestTVShowsByGenre){
+    if(bestTVShowsByGenre[key] === show) {
+     return key
+   }
+  }
+}
+
+
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
