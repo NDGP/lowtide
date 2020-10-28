d38cefb05e084f92d2910c7173e24f6c13276db7
Nathan <nathanpatrick939@gmail.com>
2020-10-28 23:02:25 +0000

    nvm now it works

--- eqObjects.js
+++ eqObjects.js
@@ -6,14 +6,34 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqObjects = function(object1, object2) {
-  if (Object.keys(object1) === Object.keys(object2) && Object.values(object1) === Object.values(object2)){
-    console.log(`Assertion Passed: [object1] === [object2]`)
-  }else{
-    console.log(`Assertion Failed: [object1] !== [object2]`)
+  let arrKeys = Object.keys(object2)
+  let arrValues = Object.values(object2)
+  
+  if (Object.keys(object1).length !== arrKeys.length){
+    return false
   }
-};
+
+  for (let keys of arrKeys){
+    //console.log(keys)
+    if (object1[keys]){
+      return true
+    }
+  }
+  for (let key in object1){
+    for (let values of arrValues){
+      //console.log(key)
+      if(object1[key] === arrValues[values]){
+        return true
+      }
+    }
+  }
+  return false
+}
 
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-eqObjects(ab, ba);
+console.log(eqObjects(ab, ba)); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+console.log(eqObjects(ab, abc));
